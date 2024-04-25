d1f4c17730b9220779b8a718d18e99a074b984a8
Øyvind Thune <oyvlei@gmail.com>
2024-04-25 17:50:24 +0200

    Update themeUtils.ts

--- src/utils/themeUtils.ts
+++ src/utils/themeUtils.ts
@@ -64,7 +64,7 @@ export const buildColorScale = (color: CssColor, mode: modeType) => {
   } else if (mode === "dark") {
     lightnessScale = [14, 10, 10, 15, 20, 30, 40, 50, 65, 80];
   } else {
-    lightnessScale = [7, 1, 13, 18, 23, 35, 45, 55, 75, 90];
+    lightnessScale = [7, 1, 5, 10, 15, 35, 45, 55, 75, 90];
   }
 
   let leoColors = new Color({
